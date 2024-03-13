13df65321775daab905e6e8f79376831f5fe586d
Mahesh Pansare <138249606+epic-croswords@users.noreply.github.com>
2024-03-14 00:24:58 +0530

    Create stop_container.sh

---
+++ stop_container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+
+echo "check whether any container is running"
+Container-id=docker ps | awk -F " " '{Print $1}'
+
+echo "remove the container"
+docker rm  -f $Container-id
+
+echo "check the left containers"
+docker ps 
+
